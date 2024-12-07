725ad2d4d1c03ddc5b5f86e1437740605a720a0d
Stas Sl <s.slunkov@gmail.com>
2024-12-07 06:40:13 +0300

    update pages

---
+++ .output/public/nuxt/3EZS_1_M.js
@@ -0,0 +1,1 @@
+import{_ as r,aJ as t}from"./DAUlzYO1.js";const s={};function a(e,n){return t(e.$slots,"default")}const _=r(s,[["render",a]]);export{_ as default};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import { md3 } from 'vuetify/blueprints'
+import { md3 } from "vuetify/blueprints";
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["vuetify-nuxt-module"],
   ssr: false,
+  app: {
+    buildAssetsDir: "/nuxt/"
+  },
   vuetify: {
     moduleOptions: {
       /* module specific options */
     },
     vuetifyOptions: {
       theme: {
-        defaultTheme: 'dark'
+        defaultTheme: "dark"
       }
       // blueprint: md3
       /* vuetify options */
